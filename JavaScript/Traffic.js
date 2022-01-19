eda40b8ebb056023cb8b08146d5c666deed4d0c6
ramzan1813 <ramzanniazi85@outlook.com>
2022-01-19 13:35:29 +0500

    Ready for deployment

--- JavaScript/Traffic.js
+++ JavaScript/Traffic.js
@@ -31,3 +31,34 @@ var overlayBg = document.getElementById("myOverlay");
 //Multiple Line Chart
 
 //Responsive Chart
+google.charts.load("current", { packages: ["corechart", "bar"] });
+google.charts.setOnLoadCallback(drawBarColors);
+
+function drawBarColors() {
+  var data = google.visualization.arrayToDataTable([
+    ["City", "2010 Tax", "2000 Tax"],
+    ["Lahore", 8150500, 6008000],
+    ["Islamabad", 3792000, 3694000],
+    ["Rawalpind", 2695000, 2896000],
+    ["Karach", 7099000, 3953000],
+    ["Pishwar", 1526000, 1517000],
+  ]);
+
+  var options = {
+    title: "Tax Report of Pakistan. Cities",
+    chartArea: { width: "50%" },
+    colors: ["#b0120a", "#ffab91"],
+    hAxis: {
+      title: "Tax Report",
+      minValue: 0,
+    },
+    vAxis: {
+      title: "City",
+    },
+  };
+  var chart = new google.visualization.BarChart(
+    document.getElementById("chart_div")
+  );
+  chart.draw(data, options);
+}
+//
